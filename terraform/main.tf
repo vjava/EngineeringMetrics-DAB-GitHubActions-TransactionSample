d172d3b28eb18ec6a355f82e8bea1b29403acf06
Vidya <vidya4java@gmail.com>
2026-07-06 01:33:11 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 resource "databricks_schema" "landing" {
   catalog_name = var.catalog_name
-  name = "landing"
+  name         = "landing"
 }
 
 resource "databricks_volume" "transactions" {
   catalog_name = var.catalog_name
-  schema_name = databricks_schema.landing.name
-  name = "transactions"
-  volume_type = "MANAGED"
+  schema_name  = databricks_schema.landing.name
+  name         = "transactions"
+  volume_type  = "MANAGED"
 }
